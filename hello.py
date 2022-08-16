cd2760b07d283da420643e93c6d4f5572c515a41
blue <swastiksrichandan@gmail.com>
2022-08-16 17:03:29 +0530

    added ignore and api commit

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from sanic import Sanic
 from sanic.response import json
 
-app = Sanic("my-hello-world-app")
+app = Sanic("welcome")
 
 @app.route('/')
 async def test(request):
-    return json({'hello': 'world'})
+    return json({'hello': 'api'})
 
 if __name__ == '__main__':
     app.run()
